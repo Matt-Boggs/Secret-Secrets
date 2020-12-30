56f79597b4b1bbc978b75182bfc28ea258635143
Matt Boggs <matt.david.boggs@gmail.com>
2020-12-30 14:45:54 -0700

    actually fleshing out the example

--- client/src/pages/Example.js
+++ client/src/pages/Example.js
@@ -1,18 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "../components/Grid";
+import API from "../utils/API";
+import "./example.css"
 
 
 const Example = ()=>{
+    const [users, setUsers] = useState([])
+    
+    useEffect(()=>{
+        loadUsers()
+    },[]);
+
+    const loadUsers = ()=>{
+        API.getUsers()
+        .then(res =>{
+            setUsers(res.data)
+        }).catch(err=>console.log(err))
+    };
 
     
+
     return (
-        <>
-         <div className="bla">
-        
-         </div>
-        </>
+        <Container fluid>
+            <Row>
+                <Col size="md-3">
+                    <div className="userList">
+                    <Row>
+                        <ul>
+                            {users.map(user => (
+                                <li className="col-12" key={user._id}>
+                                    {user.name}
+                                </li>
+                            ))}
+                        </ul>
+                    </Row>
+                    </div>
+                </Col>
+                <Col size = "md-9">
+                    <Row>
+                        <Col size="12">
+                            <div className="card">
+                                <h1 className="card-header">Info</h1>
+                                <span className="card-body">lorem ipsum</span>
+                            </div>
+                        </Col>
+                        <Col size="12">
+                            <div className="card">
+                                <h1 className="card-header">Info</h1>
+                                <span className="card-body">lorem ipsum</span>
+                            </div>
+                        </Col>
+                        <Col size="12">
+                            <div className="card">
+                                <h1 className="card-header">Info</h1>
+                                <span className="card-body">lorem ipsum</span>
+                            </div>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
     )
 
 
 }
 
-module.exports = Example
+export default Example
