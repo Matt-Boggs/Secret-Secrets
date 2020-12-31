8bd08e9681ca6384a31a8331dfb5a1deb12425de
Matt Boggs <matt.david.boggs@gmail.com>
2020-12-31 14:08:09 -0700

    created secrets controller and secret model

---
+++ controllers/secretsController.js
@@ -0,0 +1,38 @@
+const db = require("../models/index");
+
+// Defining methods for the SecretsController
+module.exports = {
+  findAll: function(req, res) {
+    console.log(req.query)
+    db.Secret
+      .find(req.query)
+      // .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Secret
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Secret
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Secret
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Secret
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
---
+++ models/secret.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const secretSchema = new Schema({
+    secret: {type: String},
+    access: {type: Array}
+})
+
+const Secret = mongoose.model("Secret",secretSchema)
+
+module.exports = Secret
