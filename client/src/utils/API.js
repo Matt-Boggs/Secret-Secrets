74e75bad56b10c5ea621fe989a48afa4b59737bd
Matt Boggs <matt.david.boggs@gmail.com>
2020-12-31 14:58:15 -0700

    methods for secrets route

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -16,5 +16,19 @@ export default {
   // Saves a User to the database
   saveUser: function(userData) {
     return axios.post("/api/users", userData);
+  },
+  //============================================
+
+  getSecrets: function() {
+    return axios.get("/api/secrets");
+  },
+  getSecret: function(id) {
+    return axios.get("/api/secrets/" + id);
+  },
+  deleteSecret: function(id) {
+    return axios.delete("/api/secrets/" + id);
+  },
+  saveSecret: function(secretData) {
+    return axios.post("/api/secrets", secretData);
   }
 };
