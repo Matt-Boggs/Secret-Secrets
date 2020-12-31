e6fc715d1f7f0e10edd6b0117d6ed8db22641460
Matt Boggs <matt.david.boggs@gmail.com>
2020-12-31 14:55:42 -0700

    seed reflects new structure

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const db = require("../models");
+const db = require("../models/index");
 
 // This file empties the Books collection and inserts the books below
 
@@ -11,51 +11,63 @@ mongoose.connect(
 const userSeed = [
   {
     name: "Matt",
-    secrets: [
-      {
-        // content of the secret
-        secret: "bla bla secret 1 matt",
-        // which _id's have access to the secret
-        access: [1]
-      }
-    ]
+    _id: 1
   },
   {
     name: "John",
-    secrets: [
-      {
-        secret: "bla bla secret 1 john",
-        access: [2,1]
-      },
-      {
-        secret: "bla bla secret 2 john",
-        access: [2,1,3]
-      }
-    ]
+    _id: 2
   },
   {
-    name: "Jill",
-    secrets: [
-      {
-        secret: "bla bla secret 1 jill",
-        access: [3,2]
-      },
-      {
-        secret: "bla bla secret 2 jull",
-        access: [3,1]
-      },
-      {
-        secret: "bla bla secret 3 jill",
-        access: [3]
-      }
-    ]
+    name: "Jill"
+    // 'Jill' will have an objectID
   }
   
+];
+
+const secretSeed = [
+    {
+      // content of the secret
+      secret: "bla bla secret 1 matt",
+      // which _id's have access to the secret
+      access: [1]
+    },
+    {
+      secret: "bla bla secret 1 john",
+      access: [2,1]
+    },
+    {
+      secret: "bla bla secret 2 john",
+      access: [2,1,"5fee44625bf67c45c4b2d2cd"]
+    },
+    {
+      secret: "bla bla secret 1 jill",
+      access: ["5fee44625bf67c45c4b2d2cd",2]
+    },
+    {
+      secret: "bla bla secret 2 jull",
+      access: ["5fee44625bf67c45c4b2d2cd",1]
+    },
+    {
+      secret: "bla bla secret 3 jill",
+      access: ["5fee44625bf67c45c4b2d2cd"]
+    }
 ];
 
 db.User
   .remove({})
   .then(() => db.User.collection.insertMany(userSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+
+db.Secret
+  .remove({})
+  .then(() => db.Secret.collection.insertMany(secretSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
