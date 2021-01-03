11f3e6b02be5920c92f8cc57763e9adb17e930ff
Matt Boggs <matt.david.boggs@gmail.com>
2021-01-02 19:06:13 -0700

    bye bye smiley

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -8,13 +8,9 @@ function NoMatch() {
       <Row>
         <Col size="md-12">
           <Jumbotron>
+            <h1>Error 404</h1>
             <h1>One of us did something wrong!</h1>
             <h4>I hope it was you</h4>
-            <h1>
-              <span role="img" aria-label="Face With Rolling Eyes Emoji">
-                🙄
-              </span>
-            </h1>
           </Jumbotron>
         </Col>
       </Row>
